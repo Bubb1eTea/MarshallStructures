c2ce873b6eff76e7bed0e2c1079e3e7483deb61f
Yen Ting Lin <ytlin6@student.monash.edu>
2021-08-10 21:32:54 +1000

    Formatted add page except Associate Company and Projects

--- templates/Associates/add.php
+++ templates/Associates/add.php
@@ -2,7 +2,7 @@
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Associate $associate
- * @var \Cake\Collection\CollectionInterface|string[] $companys
+ * @var \Cake\Collection\CollectionInterface|string[] $associatecompanys
  * @var \Cake\Collection\CollectionInterface|string[] $projects
  */
 ?>
@@ -10,7 +10,7 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Associate'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Associates'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
@@ -19,14 +19,14 @@
             <fieldset>
                 <legend><?= __('Add Associate') ?></legend>
                 <?php
-                    echo $this->Form->control('firstname', ['label' =>"First Name"]);
-                    echo $this->Form->control('lastname', ['label' =>"Last Name"]);
-                    echo $this->Form->control('email', ['label' =>"Email"]);
-                    echo $this->Form->control('phonenumber', ['label' =>"Phone Number"]);
-                    echo $this->Form->control('company_id', ['label' =>"Company", 'options' => $companys, 'empty' => true]);
-                    echo $this->Form->control('position', ['label' =>"Position"]);
-                    echo $this->Form->control('role', ['label' =>"Role"]);
-                    echo $this->Form->control('projects._ids', ['label' =>"Project", 'options' => $projects]);
+                    echo $this->Form->control('First Name');
+                    echo $this->Form->control('Last Name');
+                    echo $this->Form->control('Email');
+                    echo $this->Form->control('Phone Number');
+                    echo $this->Form->control('Position');
+                    echo $this->Form->control('Role');
+                    echo $this->Form->control('Associate Company', ['options' => $associatecompanys, 'empty' => false]);
+                    echo $this->Form->control('projects._ids', ['options' => $projects]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
