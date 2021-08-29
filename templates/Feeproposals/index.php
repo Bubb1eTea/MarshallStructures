6009f9aa3b6e410e680099ad9b42e4131a98778c
Samantha <scho0052@student.monash.edu>
2021-08-29 22:46:15 +1000

    Fee Proposal index formatted. Fee proposal "fee breakdown" has been noted as optional on add/edit pages

--- templates/Feeproposals/index.php
+++ templates/Feeproposals/index.php
@@ -4,23 +4,23 @@
  * @var \App\Model\Entity\Feeproposal[]|\Cake\Collection\CollectionInterface $feeproposals
  */
 ?>
+<?php session_start();
+$session = $this->request->getSession();
+$session->write('previous_url', $session->read('url'));
+$session->write('url', 'feeproposals.index');
+debug($session->read('previous_url')); ?>
+
 <div class="feeproposals index content">
-    <?= $this->Html->link(__('New Feeproposal'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Feeproposals') ?></h3>
+    <?= $this->Html->link(__('New Fee Proposal'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('Fee Proposals') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('project_id') ?></th>
-                    <th><?= $this->Paginator->sort('guarantor') ?></th>
-                    <th><?= $this->Paginator->sort('fixedfee') ?></th>
-                    <th><?= $this->Paginator->sort('hourlyrate') ?></th>
-                    <th><?= $this->Paginator->sort('disbursement') ?></th>
-                    <th><?= $this->Paginator->sort('total') ?></th>
-                    <th><?= $this->Paginator->sort('totalgst') ?></th>
-                    <th><?= $this->Paginator->sort('grandtotal') ?></th>
-                    <th><?= $this->Paginator->sort('paywithinday') ?></th>
+                    <th><?= $this->Paginator->sort('id', ['label' =>"Fee Proposal ID"]) ?></th>
+                    <th><?= $this->Paginator->sort('project_id', ['label' =>"Project ID"]) ?></th>
+                    <th><?= $this->Paginator->sort('guarantor', ['label' =>"Guarantor"]) ?></th>
+                    <th><?= $this->Paginator->sort('grandtotal', ['label' =>"Grand Total"]) ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
@@ -30,17 +30,11 @@
                     <td><?= $this->Number->format($feeproposal->id) ?></td>
                     <td><?= $feeproposal->has('project') ? $this->Html->link($feeproposal->project->projectname, ['controller' => 'Projects', 'action' => 'view', $feeproposal->project->id]) : '' ?></td>
                     <td><?= h($feeproposal->guarantor) ?></td>
-                    <td><?= $this->Number->format($feeproposal->fixedfee) ?></td>
-                    <td><?= $this->Number->format($feeproposal->hourlyrate) ?></td>
-                    <td><?= $this->Number->format($feeproposal->disbursement) ?></td>
-                    <td><?= $this->Number->format($feeproposal->total) ?></td>
-                    <td><?= $this->Number->format($feeproposal->totalgst) ?></td>
                     <td><?= $this->Number->format($feeproposal->grandtotal) ?></td>
-                    <td><?= $this->Number->format($feeproposal->paywithinday) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $feeproposal->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $feeproposal->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $feeproposal->id], ['confirm' => __('Are you sure you want to delete # {0}?', $feeproposal->id)]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $feeproposal->id], ['confirm' => __('Are you sure you want to delete fee proposal #{0}?', $feeproposal->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
