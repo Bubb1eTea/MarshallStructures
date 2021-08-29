da4c22b28dd3e5f296b5774c8338fe36b73f4658
Samantha <scho0052@student.monash.edu>
2021-08-29 23:07:12 +1000

    Fee Proposal view formatted

--- templates/Feeproposals/view.php
+++ templates/Feeproposals/view.php
@@ -4,19 +4,25 @@
  * @var \App\Model\Entity\Feeproposal $feeproposal
  */
 ?>
+<?php session_start();
+$session = $this->request->getSession();
+$session->write('previous_url', $session->read('url'));
+$session->write('url', 'feeproposals.view');
+debug($session->read('previous_url')); ?>
+
 <div class="row">
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Feeproposal'), ['action' => 'edit', $feeproposal->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Feeproposal'), ['action' => 'delete', $feeproposal->id], ['confirm' => __('Are you sure you want to delete # {0}?', $feeproposal->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Feeproposals'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Feeproposal'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Edit Fee Proposal'), ['action' => 'edit', $feeproposal->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Fee Proposal'), ['action' => 'delete', $feeproposal->id], ['confirm' => __('Are you sure you want to delete fee proposal #{0}?', $feeproposal->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Fee Proposals'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Fee Proposal'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="feeproposals view content">
-            <h3><?= h($feeproposal->id) ?></h3>
+            <h3><?= 'Fee Proposal #'.($feeproposal->id) ?></h3>
             <table>
                 <tr>
                     <th><?= __('Project') ?></th>
@@ -26,16 +32,13 @@
                     <th><?= __('Guarantor') ?></th>
                     <td><?= h($feeproposal->guarantor) ?></td>
                 </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($feeproposal->id) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Fixedfee') ?></th>
+                    <th><?= __('Fixed Fee') ?></th>
                     <td><?= $this->Number->format($feeproposal->fixedfee) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Hourlyrate') ?></th>
+                    <th><?= __('Hourly Rate') ?></th>
                     <td><?= $this->Number->format($feeproposal->hourlyrate) ?></td>
                 </tr>
                 <tr>
@@ -43,36 +46,36 @@
                     <td><?= $this->Number->format($feeproposal->disbursement) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Total') ?></th>
+                    <th><?= __('Subtotal') ?></th>
                     <td><?= $this->Number->format($feeproposal->total) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Totalgst') ?></th>
+                    <th><?= __('Total GST') ?></th>
                     <td><?= $this->Number->format($feeproposal->totalgst) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Grandtotal') ?></th>
+                    <th><?= __('Grand Total') ?></th>
                     <td><?= $this->Number->format($feeproposal->grandtotal) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Paywithinday') ?></th>
+                    <th><?= __('Pay within how many days?') ?></th>
                     <td><?= $this->Number->format($feeproposal->paywithinday) ?></td>
                 </tr>
             </table>
             <div class="text">
-                <strong><?= __('Scopeofservice') ?></strong>
+                <strong><?= __('Scope of Service') ?></strong>
                 <blockquote>
                     <?= $this->Text->autoParagraph(h($feeproposal->scopeofservice)); ?>
                 </blockquote>
             </div>
             <div class="text">
-                <strong><?= __('Documentsprovided') ?></strong>
+                <strong><?= __('Documents Provided') ?></strong>
                 <blockquote>
                     <?= $this->Text->autoParagraph(h($feeproposal->documentsprovided)); ?>
                 </blockquote>
             </div>
             <div class="text">
-                <strong><?= __('Feebreakdown') ?></strong>
+                <strong><?= __('Fee Breakdown') ?></strong>
                 <blockquote>
                     <?= $this->Text->autoParagraph(h($feeproposal->feebreakdown)); ?>
                 </blockquote>
@@ -84,33 +87,23 @@
                     <table>
                         <tr>
                             <th><?= __('Id') ?></th>
-                            <th><?= __('Project Id') ?></th>
-                            <th><?= __('Feeproposal Id') ?></th>
-                            <th><?= __('Datecreated') ?></th>
-                            <th><?= __('Invdesc') ?></th>
-                            <th><?= __('Completedpercentage') ?></th>
-                            <th><?= __('Total') ?></th>
-                            <th><?= __('Totalgst') ?></th>
-                            <th><?= __('Grandtotal') ?></th>
-                            <th><?= __('Paywithinday') ?></th>
+                            <th><?= __('Date Created') ?></th>
+                            <th><?= __('Invoice Description') ?></th>
+                            <th><?= __('Grand Total') ?></th>
+                            <th><?= __('Pay within # days?') ?></th>
                             <th class="actions"><?= __('Actions') ?></th>
                         </tr>
                         <?php foreach ($feeproposal->invoices as $invoices) : ?>
                         <tr>
                             <td><?= h($invoices->id) ?></td>
-                            <td><?= h($invoices->project_id) ?></td>
-                            <td><?= h($invoices->feeproposal_id) ?></td>
                             <td><?= h($invoices->datecreated) ?></td>
                             <td><?= h($invoices->invdesc) ?></td>
-                            <td><?= h($invoices->completedpercentage) ?></td>
-                            <td><?= h($invoices->total) ?></td>
-                            <td><?= h($invoices->totalgst) ?></td>
                             <td><?= h($invoices->grandtotal) ?></td>
                             <td><?= h($invoices->paywithinday) ?></td>
                             <td class="actions">
                                 <?= $this->Html->link(__('View'), ['controller' => 'Invoices', 'action' => 'view', $invoices->id]) ?>
                                 <?= $this->Html->link(__('Edit'), ['controller' => 'Invoices', 'action' => 'edit', $invoices->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Invoices', 'action' => 'delete', $invoices->id], ['confirm' => __('Are you sure you want to delete # {0}?', $invoices->id)]) ?>
+                                <?php // $this->Form->postLink(__('Delete'), ['controller' => 'Invoices', 'action' => 'delete', $invoices->id], ['confirm' => __('Are you sure you want to delete invoice #{0}?', $invoices->id)]) ?>
                             </td>
                         </tr>
                         <?php endforeach; ?>
