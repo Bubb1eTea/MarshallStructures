d5afaaa57abddc39b2fe3f51948055dc1eeab8f9
Samantha <scho0052@student.monash.edu>
2021-08-30 00:14:24 +1000

    Invoice view page has been formatted

--- templates/Invoices/view.php
+++ templates/Invoices/view.php
@@ -4,59 +4,65 @@
  * @var \App\Model\Entity\Invoice $invoice
  */
 ?>
+<?php session_start();
+$session = $this->request->getSession();
+$session->write('previous_url', $session->read('url'));
+$session->write('url', 'invoices.view');
+debug($session->read('previous_url')); ?>
+
 <div class="row">
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
             <?= $this->Html->link(__('Edit Invoice'), ['action' => 'edit', $invoice->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Invoice'), ['action' => 'delete', $invoice->id], ['confirm' => __('Are you sure you want to delete # {0}?', $invoice->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Invoice'), ['action' => 'delete', $invoice->id], ['confirm' => __('Are you sure you want to delete invoice #{0}?', $invoice->id), 'class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('List Invoices'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('New Invoice'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="invoices view content">
-            <h3><?= h($invoice->id) ?></h3>
+            <h3><?= 'Invoice #'.h($invoice->id) ?></h3>
             <table>
                 <tr>
                     <th><?= __('Project') ?></th>
                     <td><?= $invoice->has('project') ? $this->Html->link($invoice->project->projectname, ['controller' => 'Projects', 'action' => 'view', $invoice->project->id]) : '' ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Feeproposal') ?></th>
+                    <th><?= __('Fee Proposal ID') ?></th>
                     <td><?= $invoice->has('feeproposal') ? $this->Html->link($invoice->feeproposal->id, ['controller' => 'Feeproposals', 'action' => 'view', $invoice->feeproposal->id]) : '' ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($invoice->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Completedpercentage') ?></th>
+                    <th><?= __('Completed Percentage') ?></th>
                     <td><?= $this->Number->format($invoice->completedpercentage) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Total') ?></th>
+                    <th><?= __('Subtotal') ?></th>
                     <td><?= $this->Number->format($invoice->total) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Totalgst') ?></th>
+                    <th><?= __('Total GST') ?></th>
                     <td><?= $this->Number->format($invoice->totalgst) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Grandtotal') ?></th>
+                    <th><?= __('Grand Total') ?></th>
                     <td><?= $this->Number->format($invoice->grandtotal) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Paywithinday') ?></th>
+                    <th><?= __('Pay within how many days?') ?></th>
                     <td><?= $this->Number->format($invoice->paywithinday) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Datecreated') ?></th>
-                    <td><?= h($invoice->datecreated) ?></td>
+                    <th><?= __('Date Created') ?></th>
+                    <td><?= date('D d/m/y',strtotime($invoice->datecreated)) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Date Due') ?></th>
+                    <td><?= date('D d/m/y', strtotime($invoice->datecreated. ' + ' .$invoice->paywithinday.' days')) ?></td>
                 </tr>
             </table>
             <div class="text">
-                <strong><?= __('Invdesc') ?></strong>
+                <strong><?= __('Invoice Description') ?></strong>
                 <blockquote>
                     <?= $this->Text->autoParagraph(h($invoice->invdesc)); ?>
                 </blockquote>
